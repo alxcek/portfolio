ac6056222b01c98db5ac657d14e2b3f8c08b596b
Aleksa Cekić <aleksa.cekic8@gmail.com>
2021-10-31 16:47:48 +0100

    styled header and added hamburger button to responsive design

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
 import "./header.styles.scss";
+import Hamburger from "../hamburger-button/hamburger.component";
 
 
-const Header = () => (
-    <header>
-        <div className="header">
-            <Link className="logo-container" to="/">Aleksa Cekić</Link>
-            <div className="options">
-                <Link className="option" to="/about">ABOUT</Link>
-                <Link className="option" to="/projects">PROJECTS</Link>
-                <Link className="option" to="/contact">CONTACT</Link>
+const Header = () => {
+    const [status, setStatus] = useState("close");
+
+    return (
+        <header>
+            <div className="header">
+                <div className="header__logo__container">
+                    <Link className="header__logo__container__logo" to="/">WELCOME</Link>
+                </div>
+                <div className={`header__options`}>
+                    <Link className="header__options__option" to="/about">ABOUT</Link>
+                    <Link className="header__options__option" to="/projects">PROJECTS</Link>
+                    <Link className="header__options__option" to="/contact">CONTACT</Link>
+                    <div className="header__options__hamburger" role="button" onClick={() => setStatus( status === "open" ? "close" : "open")}>
+                        <Hamburger/>
+                    </div>
+                    {/*<div className={`header__options__hamburger__list ${status}`}>*/}
+                    {/*    <Link className="header__options__hamburger__list__item" to="/about">ABOUT</Link>*/}
+                    {/*    <Link className="header__options__hamburger__list__item" to="/projects">PROJECTS</Link>*/}
+                    {/*    <Link className="header__options__hamburger__list__item" to="/contact">CONTACT</Link>*/}
+                    {/*</div>*/}
+                </div>
             </div>
-            <div className="hamburger">
-
-            </div>
-        </div>
-    </header>
-)
+        </header>
+    )
+}
 
 export default Header;
