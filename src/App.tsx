1b809aea30044bec67389f46ef713dc4439d8f1f
Aleksa Cekić <aleksa.cekic8@gmail.com>
2021-11-19 14:33:18 +0100

    finished home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import './App.scss';
 import Loader from "./components/loader/loader.component";
 import {Switch} from "react-router-dom";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/home-page/home-page.component";
+
+import "./App.scss";
 
 function App() {
     const [loading, setLoading] = useState(false);
--- src/pages/home-page/home-page.component.tsx
+++ src/pages/home-page/home-page.component.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import "./home-page.styles.scss";
 
 
@@ -8,11 +9,18 @@ const HomePage = () => {
             <div className="home-page__text">
                 <h2>
                     Aleksa Cekić
-                    <br/>
-                    <span/>
                 </h2>
+                <span/>
             </div>
-
+            <div className="home-page__description">
+                <p className="home-page__description__desc">
+                    Student that loves to learn new stuff and explore new challenges. Very passionate about breaking down complex problems into simple solutions.
+                </p>
+                <br/>
+                <p className="home-page__description__text">
+                    Intern at ISUM @ Metropolitan University • Software Engineer
+                </p>
+            </div>
         </section>
     );
 }
