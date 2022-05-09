51d0ccf7e6cba9e26437fb6b65f5a6d6c50bef38
Aleksa <aleksa.cekic8@gmail.com>
2022-05-10 00:58:27 +0200

    added overview

--- src/pages/isum/isum.component.tsx
+++ src/pages/isum/isum.component.tsx
@@ -24,7 +24,8 @@ const Isum = () => {
                             </div>
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
                             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                            mollit anim id est laborum.
+                            mollit anim id est laborum. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat.
                         </div>
                     </div>
                 </div>
