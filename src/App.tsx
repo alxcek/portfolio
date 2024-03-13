c7a914d39035b5629cb27037e372e8a17849073f
Aleksa Cekić <aleksa.cekic8@gmail.com>
2024-03-13 20:41:05 +0100

    updated versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
-import React, { useEffect, useState } from "react";
-// import Loader from "./components/loader/loader.component";
-import { Route, Switch } from "react-router-dom";
-// import Header from "./components/header/header.component";
+import React from "react";
+import {Routes, Route} from "react-router-dom";
 import HomePage from "./pages/home-page/home-page.component";
-
 import "./App.scss";
-// import AboutPage from "./pages/about/about.component";
 import Isum from "./pages/isum/isum.component";
 import Esl from "./pages/esl/esl.component";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 5500);
-  }, []);
-
-  return (
-    <React.Fragment>
-      {/*{*/}
-      {/*    loading ? (*/}
-      {/*        <div className="loader">*/}
-      {/*            <Loader/>*/}
-      {/*        </div>*/}
-      {/*    ) : (*/}
-      <div className="main">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/isum" component={Isum} />
-          <Route path="/esl" component={Esl} />
-        </Switch>
-      </div>
-      {/*    )*/}
-      {/*}*/}
-    </React.Fragment>
-  );
+    return (
+        <div className="main">
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/isum" element={<Isum/>}/>
+                <Route path="/esl" element={<Esl/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
