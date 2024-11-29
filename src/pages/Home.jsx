f4200bb62fc15194057978f5a890f50211ad7c48
Aleksa Cekic <aleksa.cekic8@gmail.com>
2024-11-29 15:30:12 +0100

    tailwind finally working

---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => (
+    <div className="h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
+)
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
