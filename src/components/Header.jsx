36eba544dc962119dc5b6c234d5174abd1b5424e
Aleksa Cekic <aleksa.cekic8@gmail.com>
2024-12-24 22:24:06 +0100

    changed header a bit and working on projects tab now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav className="sticky z-50 top-0 flex bg-[#111] flex-col sm:flex-row items-center border-b border-white/15 justify-between px-4 py-2 text-sm shadow-sm">
-      <div className="flex-shrink-0 font-light text-lg">Aleksa Cekić</div>
-      <div className="flex flex-col sm:flex-row justify-center sm:gap-96 mx-auto mt-4 sm:mt-0">
-        <div>Software Engineer</div>
-        <div>Niš, Serbia</div>
+    <nav className="sticky z-50 top-0 flex bg-[#111] items-center border-b border-white/15 justify-between px-4 py-2 text-sm shadow-sm">
+      <div className="flex-shrink-0 font-light text-lg ml-8">
+        Aleksa Cekić
+      </div>
+
+      <div className="flex-grow text-center">
+        Software Engineer
+      </div>
+
+      <div className="flex-shrink-0 mr-8">
+        Niš, Serbia
       </div>
     </nav>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,34 @@
 const Projects = () => {
   const projects = [
-    { id: 1, image: "https://via.placeholder.com/400x300", title: "Project 1", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { id: 2, image: "https://via.placeholder.com/300x300", title: "Project 2", description: "This is the second project." },
-    { id: 3, image: "https://via.placeholder.com/300x500", title: "Project 3", description: "This is the third project." },
-    { id: 4, image: "https://via.placeholder.com/500x400", title: "Project 4", description: "This is the fourth project." },
-    { id: 5, image: "https://via.placeholder.com/500x400", title: "Project 5", description: "This is the fifth project." },
-    { id: 6, image: "https://via.placeholder.com/500x400", title: "Project 6", description: "This is the sixth project." },
+    { id: 1, image: "https://via.placeholder.com/500x400", title: "Project 4", description: "This is the fourth project." },
+    { id: 2, image: "https://via.placeholder.com/500x400", title: "Project 5", description: "This is the fifth project." },
+    { id: 3, image: "https://via.placeholder.com/500x400", title: "Project 6", description: "This is the sixth project." },
+    { id: 4, image: "https://via.placeholder.com/500x400", title: "Project 6", description: "This is the sixth project." },
+    { id: 5, image: "https://via.placeholder.com/500x400", title: "Project 4", description: "This is the fourth project." },
+    { id: 6, image: "https://via.placeholder.com/500x400", title: "Project 5", description: "This is the fifth project." },
+    { id: 7, image: "https://via.placeholder.com/500x400", title: "Project 6", description: "This is the sixth project." },
+    { id: 8, image: "https://via.placeholder.com/500x400", title: "Project 6", description: "This is the sixth project." },
   ];
 
   return (
      <section className="bg-[#111] text-gray-500 min-h-screen px-1 py-8">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
-        {projects.map((project, index) => (
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-1">
+        {projects.map((project) => (
           <div key={project.id} className="flex flex-col gap-2">
-            <div className="group relative overflow-hidden rounded-lg transform transition-transform duration-700 hover:scale-105 hover:z-10 hover:shadow-lg">
+            <div className="group relative overflow-hidden rounded-lg">
               <div className="relative w-full">
                 <img
                   src={project.image}
                   alt={project.title}
-                  className="w-full h-auto object-cover rounded-t-lg"
+                  className="w-full h-auto object-cover rounded-t-lg transition-all duration-300 group-hover:opacity-70"
                 />
-              </div>
-              <div className="absolute bottom-0 w-full p-4 text-gray-200 bg-opacity-75 rounded-b-lg">
-                <p className="text-sm m-0">{project.description}</p>
+                <div className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300 opacity-0 group-hover:opacity-100 flex items-center justify-center hover:cursor-pointer">
+                  <div className="text-white text-center">
+                    <h3 className="text-lg font-semibold">{project.title}</h3>
+                    <p className="text-sm">{project.description}</p>
+                    <p className="text-sm">View</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
