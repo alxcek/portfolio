1d18580d8cfad12814e0f0b570209adec3209680
Aleksa Cekic <aleksa.cekic8@gmail.com>
2025-03-04 21:41:38 +0100

    fixed scroll

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
-import e1 from '../../public/assets/e1.png';
-import u1 from '../../public/assets/u1.png';
+import e1 from "../../public/assets/e1.png";
+import u1 from "../../public/assets/u1.png";
 
 const Projects = () => {
   return (
@@ -44,25 +44,6 @@ const Projects = () => {
             </div>
           </div>
         </div>
-        <div className="cursor-pointer grid w-full opacity-100 select-none group">
-          <div className="relative group-hover:opacity-80">
-            <img src="https://placehold.co/950x400" alt="test" />
-            <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity">
-              <span className="text-white text-lg mb-4">Prompt Text Here</span>
-              <div className="flex gap-2">
-                <button className="bg-white text-black px-4 py-2 rounded">
-                  Button 1
-                </button>
-                <button className="bg-white text-black px-4 py-2 rounded">
-                  Button 2
-                </button>
-                <button className="bg-white text-black px-4 py-2 rounded">
-                  Button 3
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
       {/* COL - 2 */}
       <div className="flex flex-col gap-6">
@@ -88,25 +69,6 @@ const Projects = () => {
         <div className="cursor-pointer grid w-full opacity-100 select-none group">
           <div className="relative group-hover:opacity-80">
             <img src={u1} alt="test" />
-            <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity">
-              <span className="text-white text-lg mb-4">Prompt Text Here</span>
-              <div className="flex gap-2">
-                <button className="bg-white text-black px-4 py-2 rounded">
-                  Button 1
-                </button>
-                <button className="bg-white text-black px-4 py-2 rounded">
-                  Button 2
-                </button>
-                <button className="bg-white text-black px-4 py-2 rounded">
-                  Button 3
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="cursor-pointer grid w-full opacity-100 select-none group">
-          <div className="relative group-hover:opacity-80">
-            <img src="https://placehold.co/950x600" alt="test" />
             <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity">
               <span className="text-white text-lg mb-4">Prompt Text Here</span>
               <div className="flex gap-2">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,17 @@ import Footer from "../components/Footer";
 
 const Home = () => (
   <div className="flex flex-col items-center">
-    <div className="relative w-full px-6 max-w-[1920px]">
+    <div className="w-full px-6 max-w-[1920px]">
       <Header />
-      <About />
-      <Projects />
+      <div className="relative">
+        <About />
+        <div className="relative z-10 bg-[#111]">
+          <Projects />
+        </div>
+      </div>
       <Footer />
     </div>
   </div>
 );
+
 export default Home;
