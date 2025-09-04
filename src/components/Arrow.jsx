01c1704d124fa0483ad09885d83cf7a43f24cf77
yxngalex <aleksa.cekic8@gmail.com>
2025-09-04 12:51:55 +0200

    chore: minor refactors

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
 const Arrow = () => (
-    <div className="relative">
-        <span
-            className="block w-5 h-5 border-b-2 border-r-2 border-gray-400 transform rotate-45 -m-2.5 animate-[arrowBounce_2s_infinite] pointer-events-none"
-            style={{
-                animationDelay: '0s'
-            }}
-        />
-        <span
-            className="block w-5 h-5 border-b-2 border-r-2 border-gray-400 transform rotate-45 -m-2.5 animate-[arrowBounce_2s_infinite] pointer-events-none"
-            style={{
-                animationDelay: '-0.2s'
-            }}
-        />
-        <span
-            className="block w-5 h-5 border-b-2 border-r-2 border-gray-400 transform rotate-45 -m-2.5 animate-[arrowBounce_2s_infinite] pointer-events-none"
-            style={{
-                animationDelay: '-0.4s'
-            }}
-        />
-        <style jsx>{`
+  <div className="relative">
+    <span
+      className="block w-5 h-5 border-b-2 border-r-2 border-gray-400 transform rotate-45 -m-2.5 animate-[arrowBounce_2s_infinite] pointer-events-none"
+      style={{
+        animationDelay: '0s'
+      }}
+    />
+    <span
+      className="block w-5 h-5 border-b-2 border-r-2 border-gray-400 transform rotate-45 -m-2.5 animate-[arrowBounce_2s_infinite] pointer-events-none"
+      style={{
+        animationDelay: '-0.2s'
+      }}
+    />
+    <span
+      className="block w-5 h-5 border-b-2 border-r-2 border-gray-400 transform rotate-45 -m-2.5 animate-[arrowBounce_2s_infinite] pointer-events-none"
+      style={{
+        animationDelay: '-0.4s'
+      }}
+    />
+    <style jsx>{`
       @keyframes arrowBounce {
         0% {
           opacity: 0;
@@ -35,7 +35,7 @@ const Arrow = () => (
         }
       }
     `}</style>
-    </div>
+  </div>
 );
 
 export default Arrow;
